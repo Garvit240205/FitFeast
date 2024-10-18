b4037171fd200ac1e54f115ee79ab43dde703545
mhimeksh <mhimeksh@gmail.com>
2024-10-18 13:24:09 +0530

    added Backend for updating user profile details

--- Backend/middleware/authMiddleware.js
+++ Backend/middleware/authMiddleware.js
@@ -1,6 +1,9 @@
 // middleware/authMiddleware.js
 const jwt = require('jsonwebtoken');
-
+// {
+//   "firstname": "garvit",
+//  "age": 19, "weight": 72.5, "height":150, "gender":"trans", "goal": "Body nhi bn paa rhi"
+// }
 // Replace 'your_jwt_secret' with an environment variable or a secure key
 const JWT_SECRET = '4a751785ea0a685ec4d98f1d25b17730bae31ad1fbf1310cda8069a90bce2b47';
 
@@ -17,7 +20,7 @@ const authenticateToken = (req, res, next) => {
     if (err) {
       return res.status(403).json({ message: 'Invalid token' });
     }
-
+    
     req.user = user;
     next();
   });
--- Backend/models/user.js
+++ Backend/models/user.js
@@ -1,12 +1,16 @@
-// models/User.js
+// models/user.js
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-});
+  firstname: { type: String },
+  age: { type: Number },
+  weight: { type: Number },
+  height: { type: Number },
+  gender: { type: String },
+  goal: { type: String },
+}, { timestamps: true });
 
 const User = mongoose.model('User', userSchema);
 module.exports = User;
-
-// hello 
