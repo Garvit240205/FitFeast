0528e3b0b49a6d477040a7b31d23954219238eec
mhimeksh <mhimeksh@gmail.com>
2024-10-22 15:24:22 +0530

    Uncommited changes

--- CGAS-Project-main/src/main.jsx
+++ CGAS-Project-main/src/main.jsx
@@ -1,3 +1,4 @@
+<<<<<<< ours
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import {SignInPage} from './SignIn.jsx'
@@ -16,4 +17,49 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <FitnessProfile />
   </StrictMode>
+||||||| ancestor
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import {SignInPage} from './SignIn.jsx'
+import {SignUpPage} from './SignUp.jsx'
+import Profile from './Profile.jsx'
+import Home from './Home.jsx'
+import Explore from './explore.jsx'
+import App from './App.jsx'
+import WelcomePage from './WelcomePage.jsx'
+import FitnessProfile from './FitnessProfile.jsx'
+import Temp from './temp.jsx'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+<<<<<<< ours
+    <Profile />
+||||||| ancestor
+    <FitnessProfile />
+=======
+    <Home />
+>>>>>>> theirs
+  </StrictMode>,
+=======
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import {SignInPage} from './SignIn.jsx'
+import {SignUpPage} from './SignUp.jsx'
+import Profile from './Profile.jsx'
+import Home from './Home.jsx'
+import Explore from './explore.jsx'
+import App from './App.jsx'
+import WelcomePage from './WelcomePage.jsx'
+import FitnessProfile from './FitnessProfile.jsx'
+import Temp from './temp.jsx'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <Profile />
+  </StrictMode>,
+>>>>>>> theirs
 )
