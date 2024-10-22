cbea7a276fcfd3058f9e625fcec76da080d71781
Garvit+Aditya(Ap Project) <garvit22185@iiitd.ac.in>
2024-10-22 15:08:43 +0530

    bc

--- CGAS-Project-main/src/main.jsx
+++ CGAS-Project-main/src/main.jsx
@@ -1,48 +1,3 @@
-<<<<<<< ours
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import {SignInPage} from './SignIn.jsx'
-import {SignUpPage} from './SignUp.jsx'
-import Profile from './Profile.jsx'
-import Home from './Home.jsx'
-import Explore from './explore.jsx'
-import App from './App.jsx'
-import WelcomePage from './WelcomePage.jsx'
-import FitnessProfile from './FitnessProfile.jsx'
-import Temp from './temp.jsx'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.css'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-<<<<<<< ours
-    <Profile />
-||||||| ancestor
-    <FitnessProfile />
-=======
-    <Home />
->>>>>>> theirs
-  </StrictMode>,
-||||||| ancestor
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import {SignInPage} from './SignIn.jsx'
-import {SignUpPage} from './SignUp.jsx'
-import Profile from './Profile.jsx'
-import Home from './Home.jsx'
-import Explore from './explore.jsx'
-import App from './App.jsx'
-import WelcomePage from './WelcomePage.jsx'
-import FitnessProfile from './FitnessProfile.jsx'
-import Temp from './temp.jsx'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.css'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <Profile />
-  </StrictMode>,
-=======
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import {SignInPage} from './SignIn.jsx'
@@ -60,6 +15,5 @@ import './index.css'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <FitnessProfile />
-  </StrictMode>,
->>>>>>> theirs
+  </StrictMode>
 )
